0b11d9e108e0c56b1682fc5119cd00bbbb9e84c4
Elena <32273646+ElenaShu@users.noreply.github.com>
2021-05-26 21:05:13 +0300

    Delete Constans.swift

--- YaStockPrices/Supporting files/Constans.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Constans.swift
-//  YaStockPrices
-//
-//  Created by Elenasshu on 18.03.2021.
-//
-
-let apiKey = "pk_8552123b938f460797209e28d210c4e5"
-let apiKeyFinnhub = "c191cf748v6rd7oucqhg"
